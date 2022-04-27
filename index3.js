4b80f13f31f87e9871cd1c0e740a7f9ef15bc0b0
watabe hikari <hikari>
2022-04-27 15:08:55 +0900

    index3.jsの追加

---
+++ index3.js
@@ -0,0 +1,18 @@
+
+function confirmed(fn) {
+    const input =window.prompt("実行しますか？");
+　　fn(input);
+ }
+
+confirmed(function(input){
+    if(input　===　"実行"){
+    console.log("リポジトリを削除");
+    }
+});
+
+const foods=["にんじん","玉ねぎ","なす"];
+
+foods.forEach(function(foods){
+    console.log(foods);
+});
+
